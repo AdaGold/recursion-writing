07ecac0bf06918d4097b3d015dd6c584faf52d45
shrutivanw <shruti.dalvi@gmail.com>
2017-10-17 14:03:03 -0700

    Update recursive-methods.rb
    Updating tests and function prototypes for the exercise

--- recursive-methods.rb
+++ recursive-methods.rb
@@ -1,14 +1,34 @@
-
+# Authoring recursive algorithms. Add comments including time and space complexity for each method.
 def factorial(n)
+  puts "Not implemented."
 end
 
 def reverse(s)
+  puts "Not implemented."
+end
+
+def reverse_inplace(s)
+  puts "Not implemented."
 end
 
 def bunny(n)
+  puts "Not implemented."
 end
 
 def nested(s)
+  puts "Not implemented."
+end
+
+def search(array, value)
+  puts "Not implemented."
+end
+
+def is_palindrome(s)
+  puts "Not implemented."
+end
+
+def digit_match(n, m)
+  puts "Not implemented."
 end
 
 # Factorial Tests
@@ -21,6 +41,15 @@ raise "reverse broke - reverse('hello world')" unless reverse("hello world") == 
 raise "reverse broke - reverse('a b c')" unless reverse("a b c") == "c b a"
 puts "passes all reverse tests"
 
+# Reverse In place Tests
+str_1 = 'hello world'
+reverse_inplace(str_1)
+raise "reverse broke - reverse_inplace('hello world')" unless str_1 == "dlrow olleh"
+str_2 = 'a b c'
+reverse_inplace(str_2)
+raise "reverse broke - reverse_inplace('a b c')" unless str_2 == "c b a"
+puts "passes all reverse_inplace tests"
+
 # Bunny Tests
 raise "bunny broke - bunny(0)" unless bunny(0) == 0
 raise "bunny broke - bunny(10)" unless bunny(10) == 20
@@ -31,4 +60,23 @@ raise "nested broke - nested('((()))')" unless reverse("((()))") == true
 raise "nested broke - nested('())')" unless reverse("())") == false
 puts "passes all nested tests"
 
+# Search Tests
+arr_1 = [34, 45, 21, 4, 67]
+raise "search broke - search([34, 45, 21, 4, 67], 4)" unless search(arr_1, 4) == true
+arr_2 = [24, 51, 11]
+raise "search broke - search([24, 51, 11], 4)" unless search(arr_2, 4) == false
+arr_3 = []
+raise "search broke - search([], 4)" unless search(arr_3, 4) == false
+puts "passes all search tests"
+
+# Palindrome Tests
+raise "is_palindrome broke - is_palindrome('racecar')" unless is_palindrome('racecar') == true
+raise "is_palindrome broke - is_palindrome('smile')" unless is_palindrome('smile') == false
+puts "passes all palindrome tests"
+
+# Digit Match Tests
+raise "digit_match broke - digit_match(1072503891, 62530841)" unless digit_match(1072503891, 62530841) == 4
+raise "digit_match broke - digit_match(1234, 4321)" unless digit_match(1234, 4321) == 0
+raise "digit_match broke - digit_match(3862947593, 3862947593)" unless digit_match(3862947593, 3862947593) == 10
+puts "passes all digit_match tests"
 puts "All test passed"
